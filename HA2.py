c817d503c5b2967d6b87af8baabe288e8cdbfa92
MikaelBergquist <bergquist.mikael@gmail.com>
2018-11-22 08:44:03 +0100

    ha2 klar, läser filen dc.txt

--- HA2.py
+++ HA2.py
@@ -1,17 +1,21 @@
 import HA0
-in_sa = "27C2"
-in_sb = "0879"
-in_da = "35F6"
-in_db = "1A4D"
-in_m = "27BC"
-in_b = "1"
 
-sa = HA0.hex_to_int(in_sa)
-da = HA0.hex_to_int(in_da)
-sb = HA0.hex_to_int(in_sb)
-db = HA0.hex_to_int(in_db)
-m = HA0.hex_to_int(in_m)
-b = HA0.hex_to_int(in_b)
+_sa = "27C2"
+_sb = "0879"
+_da = "35F6"
+_db = "1A4D"
+_m = "27BC"
+_b = "1"
+
+file = list(map(lambda x: str.split(x)[1], HA0.parse_file("dc.txt")))
+[_sa, _sb, _da, _db, _m, _b] = file
+
+sa = HA0.hex_to_int(_sa)
+da = HA0.hex_to_int(_da)
+sb = HA0.hex_to_int(_sb)
+db = HA0.hex_to_int(_db)
+m = HA0.hex_to_int(_m)
+b = HA0.hex_to_int(_b)
 
 if(b == 0):
     broadcasted = HA0.int_to_hex(sa^sb)
